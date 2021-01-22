1d482f5aa0252d337f0de6a082d363e9e1791bf3
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-01-22 11:29:58 -0500

    Ajout 3 components pour choix couleur

---
+++ client/src/app/components/colour-palette-selector/colour-palette-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colour-palette-selector',
+  templateUrl: './colour-palette-selector.component.html',
+  styleUrls: ['./colour-palette-selector.component.scss']
+})
+export class ColourPaletteSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/components/colour-selector/colour-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colour-selector',
+  templateUrl: './colour-selector.component.html',
+  styleUrls: ['./colour-selector.component.scss']
+})
+export class ColourSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
