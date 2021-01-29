5ad2e4d0678fe856e7e18985eb1f8c50a6c79ae3
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-01-29 11:58:16 -0500

    La taille initiale de la surface de dessin est la moité de la zone de travail au moment de la création.

--- client/src/app/components/drawing/drawing.component.ts
+++ client/src/app/components/drawing/drawing.component.ts
@@ -1,12 +1,23 @@
 import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Tool } from '@app/classes/tool';
-import { Vec2 } from '@app/classes/vec2';
+// import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { PencilService } from '@app/services/tools/pencil-service';
 
 // TODO : Avoir un fichier séparé pour les constantes ?
-export const DEFAULT_WIDTH = window.innerWidth / 2;
-export const DEFAULT_HEIGHT = window.innerHeight / 2;
+export const DEFAULT_WIDTH = 1000;
+export const DEFAULT_HEIGHT = 800;
+
+export const MINIMUM_WIDTH = 250;
+export const MINIMUM_HEIGHT = 250;
+
+export const LOWER_BOUND_WIDTH = 500;
+export const LOWER_BOUND_HEIGHT = 500;
+
+export const DEFAULT_WHITE = '#fff';
+// export const DEFAULT_BACKGROUND_COLOR = '#b3b3b3';
+
+export const SIDEBAR_WIDTH = 300;
 
 @Component({
     selector: 'app-drawing',
@@ -20,7 +31,7 @@ export class DrawingComponent implements AfterViewInit {
 
     private baseCtx: CanvasRenderingContext2D;
     private previewCtx: CanvasRenderingContext2D;
-    private canvasSize: Vec2 = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
+    // private canvasSize: Vec2 = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
 
     // TODO : Avoir un service dédié pour gérer tous les outils ? Ceci peut devenir lourd avec le temps
     private tools: Tool[];
@@ -36,6 +47,9 @@ export class DrawingComponent implements AfterViewInit {
         this.drawingService.baseCtx = this.baseCtx;
         this.drawingService.previewCtx = this.previewCtx;
         this.drawingService.canvas = this.baseCanvas.nativeElement;
+        this.drawingService.canvas.style.backgroundColor = DEFAULT_WHITE;
+        // this.drawingService.canvas.style.minHeight = '300';
+        // this.drawingService.canvas.style.minWidth = '300';
     }
 
     @HostListener('mousemove', ['$event'])
@@ -54,10 +68,16 @@ export class DrawingComponent implements AfterViewInit {
     }
 
     get width(): number {
-        return this.canvasSize.x;
+        if (window.innerWidth - SIDEBAR_WIDTH < LOWER_BOUND_WIDTH) {
+            return MINIMUM_WIDTH;
+        }
+        return (window.innerWidth - SIDEBAR_WIDTH) / 2;
     }
 
     get height(): number {
-        return this.canvasSize.y;
+        if (window.innerHeight < LOWER_BOUND_HEIGHT) {
+            return MINIMUM_HEIGHT;
+        }
+        return window.innerHeight / 2;
     }
 }
