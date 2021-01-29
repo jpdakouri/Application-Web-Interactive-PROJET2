9fe758228ecc224af3edb59580f97652eb9a7d97
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-01-29 14:48:06 -0500

    colourhistoryservice fini et testé

--- client/src/app/services/colour-history/colour-history.service.spec.ts
+++ client/src/app/services/colour-history/colour-history.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
+import { ColourHistoryService, HISTORY_LENGTH } from './colour-history.service';
 
-import { ColourHistoryService } from './colour-history.service';
-
+const DEFAULT_COLOUR = 'rgba(255,255,255,1)';
 describe('ColourHistoryService', () => {
-  let service: ColourHistoryService;
+    let service: ColourHistoryService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ColourHistoryService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ColourHistoryService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('upon creation, the history is filled with 10x white', () => {
+        let colouredPassed = 0;
+        for (let i = 0; i < HISTORY_LENGTH; i++) {
+            if (service.getColour(i) === DEFAULT_COLOUR) colouredPassed++;
+        }
+        expect(colouredPassed).toBe(HISTORY_LENGTH);
+    });
+
+    it('pushColour adds the new color to the front', () => {
+        const NEW_COLOUR = 'rgba(0,0,0,1)';
+        service.pushColour(NEW_COLOUR);
+        expect(service.getColour(0)).toBe(NEW_COLOUR);
+    });
+
+    it('pushColour adds the new color to the front', () => {
+        const NEW_COLOUR = 'rgba(0,0,0,1)';
+        service.pushColour(NEW_COLOUR);
+        expect(service.getColour(0)).toBe(NEW_COLOUR);
+    });
+
+    it('selectColour puts the selected color at the front and returns it', () => {
+        const WANTED_COLOUR = 'rgba(0,0,0,1)';
+        const UNWANTED_COLOUR = 'rgba(1,1,1,1)';
+        service.pushColour(WANTED_COLOUR);
+        service.pushColour(UNWANTED_COLOUR);
+        const SELECTED_COLOUR = service.selectColour(1);
+        expect(SELECTED_COLOUR).toBe(WANTED_COLOUR);
+        expect(service.getColour(0)).toBe(WANTED_COLOUR);
+        expect(service.getColour(1)).toBe(UNWANTED_COLOUR);
+    });
 });
--- client/src/app/services/colour-history/colour-history.service.ts
+++ client/src/app/services/colour-history/colour-history.service.ts
@@ -1,36 +1,38 @@
 import { Injectable } from '@angular/core';
-const HISTORY_LENGTH = 10;
+export const HISTORY_LENGTH = 10;
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ColourHistoryService {
     private colours: string[];
 
     constructor() {
-        const RBGA_WHITE = 'rgba(255,255,255,1)';
+        const DEFAULT_COLOUR = 'rgba(255,255,255,1)';
         this.colours = [];
         for (let i = 0; i < HISTORY_LENGTH; i++) {
-            this.colours.push(RBGA_WHITE);
+            this.colours.push(DEFAULT_COLOUR);
         }
     }
+
     getColour(index: number): string {
-        if (index >= 0 && index < HISTORY_LENGTH)
-            return this.colours[index];
+        if (index >= 0 && index < HISTORY_LENGTH) return this.colours[index];
         return '';
     }
-    pushColour(colour: string): void{
+
+    pushColour(colour: string): void {
         for (let i = 9; i >= 1; i--) {
-            this.colours[i] = this.colours[i-1];
+            this.colours[i] = this.colours[i - 1];
         }
-        this.colours.splice(-1,1)
+        const LAST_ELEMENT_INDEX = -1;
+        this.colours.splice(LAST_ELEMENT_INDEX, 1);
         this.colours.unshift(colour);
     }
 
-    selectColour(colour: string): void {
-        for (let i = 9; i >= 1; i--) {
-            this.colours[i] = this.colours[i-1];
-        }
-        this.colours.splice(-1,1)
-        this.colours.unshift(colour);
+    selectColour(index: number): string {
+        if (index < 0 && index >= HISTORY_LENGTH) return '';
+        const SELECTED_COLOUR = this.colours[index];
+        this.colours.splice(index, 1);
+        this.colours.unshift(SELECTED_COLOUR);
+        return SELECTED_COLOUR;
     }
 }
