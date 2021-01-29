aac86b58e2ad5dd02cd9cdfd239b560bdf77ccf5
Jean-Philippe Dakouri <jean-philippe.dakouri@outlook.com>
2021-01-29 15:36:19 -0500

    Ajout du service rectangle et rename branch

---
+++ client/src/app/services/tools/rectangle.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+
+// TODO : Déplacer ça dans un fichier séparé accessible par tous
+export enum MouseButton {
+    Left = 0,
+    Middle = 1,
+    Right = 2,
+    Back = 3,
+    Forward = 4,
+}
+
+const un = 50;
+const deux = 50;
+const trois = 150;
+const quatre = 250;
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PencilService extends Tool {
+    private pathData: Vec2[];
+
+    constructor(drawingService: DrawingService) {
+        super(drawingService);
+        this.clearPath();
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        this.mouseDown = event.button === MouseButton.Left;
+        if (this.mouseDown) {
+            this.clearPath();
+
+            this.mouseDownCoord = this.getPositionFromMouse(event);
+            this.pathData.push(this.mouseDownCoord);
+        }
+    }
+
+    onMouseUp(event: MouseEvent): void {
+        if (this.mouseDown) {
+            const mousePosition = this.getPositionFromMouse(event);
+            this.pathData.push(mousePosition);
+            this.drawRectangle(this.drawingService.baseCtx, this.pathData);
+        }
+        this.mouseDown = false;
+        this.clearPath();
+    }
+
+    onMouseMove(event: MouseEvent): void {
+        if (this.mouseDown) {
+            const mousePosition = this.getPositionFromMouse(event);
+            this.pathData.push(mousePosition);
+
+            // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            this.drawRectangle(this.drawingService.previewCtx, this.pathData);
+        }
+    }
+
+    private drawRectangle(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
+        ctx.beginPath();
+        ctx.fillRect(un, deux, trois, quatre);
+    }
+
+    private clearPath(): void {
+        this.pathData = [];
+    }
+}
