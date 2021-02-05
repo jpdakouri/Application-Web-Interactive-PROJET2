98ca05a03a1bf9b73739de21671195e7f09fc352
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-05 09:28:57 -0500

    ajour colour history component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,9 +12,10 @@ import { HueSelectorComponent } from './components/hue-selector/hue-selector.com
 import { ColourPaletteSelectorComponent } from './components/colour-palette-selector/colour-palette-selector.component';
 import { FormsModule } from '@angular/forms';
 import { CurrentColorComponent } from './components/current-color/current-color.component';
+import { ColourHistoryComponent } from './components/colour-history/colour-history.component';
 
 @NgModule({
-    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent, ColourSelectorComponent, HueSelectorComponent, ColourPaletteSelectorComponent, CurrentColorComponent],
+    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent, ColourSelectorComponent, HueSelectorComponent, ColourPaletteSelectorComponent, CurrentColorComponent, ColourHistoryComponent],
     imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
