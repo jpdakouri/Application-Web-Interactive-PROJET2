bbcae0ca1917289fd93f1b5c7c3df4a1fc8c6041
Jean-Philippe Dakouri <jean-philippe.dakouri@outlook.com>
2021-02-08 15:44:49 -0500

    Tests en progrès

--- client/src/app/services/tools/rectangle.service.spec.ts
+++ client/src/app/services/tools/rectangle.service.spec.ts
@@ -1,19 +1,50 @@
 import { TestBed } from '@angular/core/testing';
-import { Vec2 } from '@app/classes/vec2';
-import { MouseButton, RectangleService } from './rectangle.service';
+import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+import { RectangleService } from './rectangle.service';
+
+export enum MouseButton {
+    Left = 0,
+    Middle = 1,
+    Right = 2,
+    Back = 3,
+    Forward = 4,
+}
 
 describe('RectangleService', () => {
     let service: RectangleService;
     let mouseEvent: MouseEvent;
+    let canvasTestHelper: CanvasTestHelper;
+    let drawServiceSpy: jasmine.SpyObj<DrawingService>;
+
+    let baseCtxStub: CanvasRenderingContext2D;
+    let previewCtxStub: CanvasRenderingContext2D;
+    // let drawRectangleSpy: jasmine.Spy<any>;
+    let updatePreviewSpy: jasmine.Spy<any>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        drawServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
+
+        TestBed.configureTestingModule({
+            providers: [{ provide: DrawingService, useValue: drawServiceSpy }],
+        });
+        canvasTestHelper = TestBed.inject(CanvasTestHelper);
+        baseCtxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+        previewCtxStub = canvasTestHelper.drawCanvas.getContext('2d') as CanvasRenderingContext2D;
+
         service = TestBed.inject(RectangleService);
+        // drawRectangleSpy = spyOn<any>(service, 'drawRectangle').and.callThrough();
+        updatePreviewSpy = spyOn<any>(service, 'drawRectangle').and.callThrough();
+
+        // Configuration du spy du service
+        // tslint:disable:no-string-literal
+        service['drawingService'].baseCtx = baseCtxStub; // Jasmine doesnt copy properties with underlying data
+        service['drawingService'].previewCtx = previewCtxStub;
 
         mouseEvent = {
-            offsetX: 30,
-            offsetY: 30,
-            button: MouseButton.Left,
+            offsetX: 25,
+            offsetY: 25,
+            button: 0,
         } as MouseEvent;
     });
 
@@ -21,9 +52,31 @@ describe('RectangleService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('should test Mouse bro', () => {
-        const expectedResult: Vec2 = { x: 30, y: 30 };
+    it('mouseDown should set value to true on left click', () => {
         service.onMouseDown(mouseEvent);
-        expect(service.mouseDownCoord).toEqual(expectedResult);
+        expect(service.mouseDown).toEqual(true);
+    });
+
+    it(' mouseDown should set mouseDown property to false on right click', () => {
+        const mouseEventRClick = {
+            offsetX: 25,
+            offsetY: 25,
+            button: 1, // TODO: Avoir ceci dans un enum accessible
+        } as MouseEvent;
+        service.onMouseDown(mouseEventRClick);
+        expect(service.mouseDown).toEqual(false);
+    });
+
+    it('onMouseMove should call previewUpdate if the drawing has started', () => {
+        service.mouseDown = true;
+        const mouseStartEvent = {
+            offsetX: 0,
+            offsetY: 0,
+            button: 1,
+        } as MouseEvent;
+        service.onMouseUp(mouseStartEvent);
+        service.onMouseMove(mouseEvent);
+
+        expect(updatePreviewSpy).toHaveBeenCalled();
     });
 });
