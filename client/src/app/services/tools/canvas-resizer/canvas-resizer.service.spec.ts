a27260951b8144e96e2ca2399d6c6237a22053ab
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-02-08 21:27:02 -0500

    ajout test #onMouseDown

--- client/src/app/services/tools/canvas-resizer/canvas-resizer.service.spec.ts
+++ client/src/app/services/tools/canvas-resizer/canvas-resizer.service.spec.ts
@@ -1,17 +1,30 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MouseHandlerService } from '@app/services/mouse-handler/mouse-handler.service';
 import { CanvasResizerService, Status } from './canvas-resizer.service';
 
 describe('CanvasResizerService', () => {
     let service: CanvasResizerService;
+    let mouseEvent: MouseEvent;
+    let mouseServiceSpy: MouseHandlerService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        mouseServiceSpy = jasmine.createSpyObj('MouseHandlerService', ['onMouseDown', 'eventToCoordinate']);
+        TestBed.configureTestingModule({
+            providers: [{ provide: MouseHandlerService, useValue: mouseServiceSpy }],
+        });
         service = TestBed.inject(CanvasResizerService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should call MouseHandlerService #onMouseDown', () => {
+        // spyOn(mouseServiceSpy, 'onMouseDown');
+        mouseEvent = {} as MouseEvent;
+        service.onMouseDown(mouseEvent);
+        expect(mouseServiceSpy.onMouseDown).toHaveBeenCalled();
+        // expect(mouseServiceSpy.onMouseDown).toHaveBeenCalledWith(mouseEvent);
     });
 
     it('should  change status when #onMiddleRightResizerClicked called', () => {
