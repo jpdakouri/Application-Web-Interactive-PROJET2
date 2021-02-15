97ed0d4bf10e7a51f55e5400d16dfcb721b8aa3d
Jean-Philippe Dakouri <jean-philippe.dakouri@outlook.com>
2021-02-14 21:08:12 -0500

    colorSelector corrections + petits fix dans css

--- client/src/app/components/components-constants.ts
+++ client/src/app/components/components-constants.ts
@@ -1,6 +1,16 @@
+// tslint:disable-next-line:prettier
 // Color-Palette-selector
 
 export const RGBA_WHITE = 'rgba(255,255,255,1)';
 export const TRANSPARENT_RGBA_WHITE = 'rgba(255,255,255,0)';
 export const RGBA_BLACK = 'rgba(0,0,0,1)';
 export const TRANSPARENT_RGBA_BLACK = 'rgba(0,0,0,0)';
+
+// color-selector
+export const PERCENTAGE_MAX = 100;
+export const MIN_HEX_RGB_VALUE = 1;
+export const MAX_HEX_RGB_VALUE = 2;
+export const INDEX_ELEMENT_NOT_THERE = -1;
+export const RGB_COMPONENTS = 3;
+export const MIN_TRANSPARENCY = 0;
+export const MAX_TRANSPARENCY = 100;
