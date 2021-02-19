fe5ac0ed4261d0d9676401968d90d79d4e09050f
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-19 14:55:28 -0500

    added case for using pipette on canvas background

--- client/src/app/services/tools/pipette-service/pipette.service.ts
+++ client/src/app/services/tools/pipette-service/pipette.service.ts
@@ -2,28 +2,29 @@ import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
 import { CurrentColourService } from '@app/services/current-colour/current-colour.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { ALPHA_INDEX, DEFAULT_CANVAS_RGB } from '@app/services/services-constants';
 import { MouseButtons } from '@app/utils/enums/list-boutton-pressed';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PipetteService extends Tool {
-    currentColourService: CurrentColourService;
-
     constructor(drawingService: DrawingService, currentColourService: CurrentColourService) {
         super(drawingService, currentColourService);
         this.currentColourService = currentColourService;
     }
 
     onMouseDown(event: MouseEvent): void {
-        const rgbAtPosition = this.getRgbAtPosition(event.offsetX, event.offsetY);
+        let rgbAtPosition = this.getRgbaAtPosition(event.offsetX, event.offsetY);
+        console.log(this.drawingService.baseCtx.fillStyle);
+        if (rgbAtPosition[ALPHA_INDEX] === '0') rgbAtPosition = DEFAULT_CANVAS_RGB;
         if (event.button === MouseButtons.Left) this.currentColourService.setPrimaryColorRgb(rgbAtPosition);
         else this.currentColourService.setSecondaryColorRgb(rgbAtPosition);
     }
 
-    private getRgbAtPosition(x: number, y: number): string {
+    private getRgbaAtPosition(x: number, y: number): string {
         const imageData = this.drawingService.baseCtx.getImageData(x, y, 1, 1).data;
         const rgbaSeperator = ',';
-        return imageData[0] + rgbaSeperator + imageData[1] + rgbaSeperator + imageData[2];
+        return imageData[0] + rgbaSeperator + imageData[1] + rgbaSeperator + imageData[2] + rgbaSeperator + imageData[ALPHA_INDEX];
     }
 }
