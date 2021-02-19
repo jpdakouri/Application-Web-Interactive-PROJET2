53c4c10cbc001a6705eb09357fcd6f13739eb4fb
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-19 12:13:28 -0500

    added base logic for left click on canvas

--- client/src/app/services/tools/pipette-service/pipette.service.ts
+++ client/src/app/services/tools/pipette-service/pipette.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Tool } from '@app/classes/tool';
+import { CurrentColourService } from '@app/services/current-colour/current-colour.service';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+import { MouseButtons } from '@app/utils/enums/list-boutton-pressed';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-export class PipetteService {
+export class PipetteService extends Tool {
+    currentColourService: CurrentColourService;
 
-  constructor() { }
+    constructor(drawingService: DrawingService, currentColourService: CurrentColourService) {
+        super(drawingService, currentColourService);
+        this.currentColourService = currentColourService;
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        const rgbAtPosition = this.getRgbAtPosition(event.offsetX, event.offsetY);
+        this.currentColourService.setPrimaryColorRgb(rgbAtPosition);
+    }
+
+    private getRgbAtPosition(x: number, y: number): string {
+        const imageData = this.drawingService.baseCtx.getImageData(x, y, 1, 1).data;
+        const rgbaSeperator = ',';
+        return imageData[0] + rgbaSeperator + imageData[1] + rgbaSeperator + imageData[2];
+    }
 }
