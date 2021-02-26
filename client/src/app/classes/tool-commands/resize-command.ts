e249cd7d2c969a83f2444c37386606a49b86038a
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-26 13:38:57 -0500

    added resize commands

--- client/src/app/classes/tool-commands/resize-command.ts
+++ client/src/app/classes/tool-commands/resize-command.ts
@@ -1,2 +1,14 @@
-export class ResizeCommand {
+import { CanvasResizerService } from '@app/services/canvas-resizer/canvas-resizer.service';
+import { ToolCommand } from '@app/utils/interfaces/tool-command';
+
+export class ResizeCommand implements ToolCommand {
+    readonly tool: CanvasResizerService;
+    readonly newWidth: number;
+    readonly newHeight: number;
+
+    constructor(tool: CanvasResizerService, newWidth: number, newHeight: number) {
+        this.tool = tool;
+        this.newWidth = newWidth;
+        this.newHeight = newHeight;
+    }
 }
--- client/src/app/utils/interfaces/tool-command.ts
+++ client/src/app/utils/interfaces/tool-command.ts
@@ -1,5 +1,6 @@
 import { Tool } from '@app/classes/tool';
+import { CanvasResizerService } from '@app/services/canvas-resizer/canvas-resizer.service';
 
 export interface ToolCommand {
-    tool: Tool;
+    tool: Tool | CanvasResizerService;
 }
