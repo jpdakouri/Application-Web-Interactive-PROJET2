156ac7ae911edfa0178aeb01e7aaf9c085cea92a
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-02-26 11:30:12 -0500

    mouse enter fonctionne

--- client/src/app/services/tools/aerosol-service/aerosol.service.ts
+++ client/src/app/services/tools/aerosol-service/aerosol.service.ts
@@ -21,31 +21,28 @@ import { MouseButtons } from '@app/utils/enums/list-boutton-pressed';
 export class AerosolService extends Tool {
     private intervalID: number;
     private mouseCurrentPosition: Vec2;
-    // private isSpraying: boolean
+    private isSpraying: boolean;
 
     constructor(drawingService: DrawingService, currentColourService: CurrentColourService) {
         super(drawingService, currentColourService);
         this.frequency = DEFAULT_FREQUENCY;
         this.dropletDiameter = DEFAULT_DROPLET_DIAMETER;
         this.jetDiameter = DEFAULT_JET_DIAMETER;
+        this.isSpraying = false;
         this.mouseCurrentPosition = { x: 0, y: 0 };
     }
 
     onMouseDown(event: MouseEvent): void {
         this.mouseDown = event.button === MouseButtons.Left;
-        if (this.mouseDown) {
-            // this.drawingService.clearCanvas(this.drawingService.previewCtx);
-            window.clearInterval(this.intervalID);
-            let frequency = this.frequency || MIN_FREQUENCY;
-            frequency /= DOTS_PER_SPRAY;
-
-            this.intervalID = window.setInterval(() => {
-                this.spray();
-            }, MS_PER_S / frequency);
+        this.isSpraying = true;
+        if (this.mouseDown && this.isSpraying) {
+            this.printSpray();
         }
     }
 
     onMouseUp(event: MouseEvent): void {
+        console.log('mouse up');
+        this.isSpraying = false;
         if (this.mouseDown) {
             window.clearInterval(this.intervalID);
             this.mouseDown = false;
@@ -56,7 +53,26 @@ export class AerosolService extends Tool {
         this.mouseCurrentPosition = this.getPositionFromMouse(event);
     }
 
-    // onMouseLeave(event: MouseEvent): void {}
+    onMouseLeave(event: MouseEvent): void {
+        console.log('mouse leave');
+        window.clearInterval(this.intervalID);
+        this.isSpraying = false;
+    }
+
+    onMouseEnter(event: MouseEvent): void {
+        console.log('mouse enter');
+        if (this.mouseDown) {
+            this.isSpraying = true;
+            this.printSpray();
+        }
+    }
+
+    async printSpray(): Promise<void> {
+        window.clearInterval(this.intervalID);
+        this.intervalID = window.setInterval(() => {
+            this.spray();
+        }, (MS_PER_S * DOTS_PER_SPRAY) / (this.frequency || MIN_FREQUENCY));
+    }
 
     spray(): void {
         this.generateSprayParticles();
@@ -67,10 +83,10 @@ export class AerosolService extends Tool {
         for (let i = 0; i < DOTS_PER_SPRAY; i++) {
             const offset = this.getRandomOffsetInRadius((this.jetDiameter || MIN_JET_DIAMETER) / 2);
             const radius = this.dropletDiameter || MIN_DROPLET_DIAMETER;
-            const currentPos = this.mouseCurrentPosition;
+            const currentPosition = this.mouseCurrentPosition;
             this.drawingService.baseCtx.strokeStyle = this.currentColourService.getPrimaryColorRgba();
             this.drawingService.baseCtx.fillStyle = this.currentColourService.getPrimaryColorRgba();
-            this.drawingService.baseCtx.fillRect(currentPos.x + offset.x, currentPos.y + offset.y, radius, radius);
+            this.drawingService.baseCtx.fillRect(currentPosition.x + offset.x, currentPosition.y + offset.y, radius, radius);
         }
     }
 
