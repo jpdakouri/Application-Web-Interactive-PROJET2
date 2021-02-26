6b6715f505fc6993ba54c77dade31c43a1e09543
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-26 13:38:27 -0500

    stricter check on pencil command

--- client/src/app/classes/tool-commands/pencil-command.ts
+++ client/src/app/classes/tool-commands/pencil-command.ts
@@ -1,5 +1,6 @@
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
+import { PencilService } from '@app/services/tools/pencil-service/pencil.service';
 import { ToolCommand } from '@app/utils/interfaces/tool-command';
 
 export class PencilCommand implements ToolCommand {
@@ -8,11 +9,10 @@ export class PencilCommand implements ToolCommand {
     readonly strokeThickness: number;
     readonly strokePath: (Vec2 | undefined)[];
 
-    constructor(tool: Tool, primaryColor: string, strokeThickness: number, strokePath: (Vec2 | undefined)[]) {
+    constructor(tool: PencilService, primaryColor: string, strokeThickness: number, strokePath: (Vec2 | undefined)[]) {
         this.tool = tool;
         this.primaryColor = primaryColor;
         this.strokeThickness = strokeThickness;
         this.strokePath = strokePath;
     }
-
 }
