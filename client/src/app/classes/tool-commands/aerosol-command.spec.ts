5361fc927c164919dd689ee42829b5146029d8e2
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-26 13:37:19 -0500

    programmed aerosol commands

--- client/src/app/classes/tool-commands/aerosol-command.spec.ts
+++ client/src/app/classes/tool-commands/aerosol-command.spec.ts
@@ -1,7 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { AerosolService } from '@app/services/tools/aerosol-service/aerosol.service';
 import { AerosolCommand } from './aerosol-command';
 
 describe('AerosolCommand', () => {
-  it('should create an instance', () => {
-    expect(new AerosolCommand()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        const tool = TestBed.inject(AerosolService);
+        expect(new AerosolCommand(tool, 'rgba(0,0,0,1)', [])).toBeTruthy();
+    });
 });
--- client/src/app/classes/tool-commands/aerosol-command.ts
+++ client/src/app/classes/tool-commands/aerosol-command.ts
@@ -1,2 +1,16 @@
-export class AerosolCommand {
+import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
+import { AerosolService } from '@app/services/tools/aerosol-service/aerosol.service';
+import { ToolCommand } from '@app/utils/interfaces/tool-command';
+
+export class AerosolCommand implements ToolCommand {
+    readonly tool: Tool;
+    readonly primaryColor: string;
+    readonly changedPixels: Vec2[];
+
+    constructor(tool: AerosolService, primaryColor: string, changedPixels: Vec2[]) {
+        this.tool = tool;
+        this.primaryColor = primaryColor;
+        this.changedPixels = changedPixels;
+    }
 }
