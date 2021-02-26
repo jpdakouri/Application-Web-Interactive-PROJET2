12bb2364c3276f24eb291299d897bbbe9d0e008f
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-26 13:36:41 -0500

    combined rectangle and ellipse commands into shape commands

--- client/src/app/classes/tool-commands/ellipse-command.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EllipseCommand } from './ellipse-command';
-
-describe('EllipseCommand', () => {
-  it('should create an instance', () => {
-    expect(new EllipseCommand()).toBeTruthy();
-  });
-});
--- client/src/app/classes/tool-commands/rectangle-command.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RectangleCommand } from './rectangle-command';
-
-describe('RectangleCommand', () => {
-  it('should create an instance', () => {
-    expect(new RectangleCommand()).toBeTruthy();
-  });
-});
