eb26c6a0afc440f54ca303e33192848da3082dba
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-02-26 17:38:06 -0500

    lint fix for test files

--- client/src/app/services/canvas-overwriter/canvas-overwriter.service.spec.ts
+++ client/src/app/services/canvas-overwriter/canvas-overwriter.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { CanvasOverwriterService } from './canvas-overwriter.service';
 
 describe('CanvasOverwriterService', () => {
-  let service: CanvasOverwriterService;
+    let service: CanvasOverwriterService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CanvasOverwriterService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(CanvasOverwriterService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
