13f17d68c42052886916eb28f572b9d48f4ea53e
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-02-26 10:08:55 -0500

    refactoring

--- client/src/app/services/tools/aerosol-service/aerosol.service.ts
+++ client/src/app/services/tools/aerosol-service/aerosol.service.ts
@@ -3,116 +3,83 @@ import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { CurrentColourService } from '@app/services/current-colour/current-colour.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
-import { MIN_AEROSOL_DROPLET_DIAMETER, MIN_AEROSOL_EMISSION_FLOW, MIN_AEROSOL_JET_DIAMETER } from '@app/services/tools/tools-constants';
+import {
+    DEFAULT_DROPLET_DIAMETER,
+    DEFAULT_FREQUENCY,
+    DEFAULT_JET_DIAMETER,
+    DOTS_PER_SPRAY,
+    MIN_DROPLET_DIAMETER,
+    MIN_FREQUENCY,
+    MIN_JET_DIAMETER,
+} from '@app/services/tools/tools-constants';
 import { MouseButtons } from '@app/utils/enums/list-boutton-pressed';
-
-// MIN_FREQUENCY = 10;
-// MAX_DROPLET_DIAMETER = 10;
 
 @Injectable({
     providedIn: 'root',
 })
 export class AerosolService extends Tool {
-    private sprayInterval: number;
-    private interval: number;
+    private intervalID: number;
+    private mouseCurrentPosition: Vec2;
+    // private isSpraying: boolean
 
     // tslint:disable:no-magic-numbers
     constructor(drawingService: DrawingService, currentColourService: CurrentColourService) {
         super(drawingService, currentColourService);
-        // this.isSpraying = false;
-        this.emissionFlow = MIN_AEROSOL_EMISSION_FLOW;
-        this.dropletDiameter = MIN_AEROSOL_DROPLET_DIAMETER;
-        this.jetDiameter = MIN_AEROSOL_JET_DIAMETER;
-        this.sprayInterval = 100;
+        this.frequency = DEFAULT_FREQUENCY;
+        this.dropletDiameter = DEFAULT_DROPLET_DIAMETER;
+        this.jetDiameter = DEFAULT_JET_DIAMETER;
+        this.mouseCurrentPosition = { x: 0, y: 0 };
     }
 
     onMouseDown(event: MouseEvent): void {
         this.mouseDown = event.button === MouseButtons.Left;
         if (this.mouseDown) {
-            this.drawingService.clearCanvas(this.drawingService.previewCtx);
-            this.mouseDownCoord = this.getPositionFromMouse(event);
-            window.clearInterval(this.interval);
-            this.spray(event, this.drawingService.baseCtx);
-            this.interval = window.setInterval(() => {
-                this.spray(event, this.drawingService.baseCtx);
-            }, this.sprayInterval);
+            // this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            window.clearInterval(this.intervalID);
+            let frequency = this.frequency || MIN_FREQUENCY;
+            frequency /= 10;
+
+            this.intervalID = window.setInterval(() => {
+                this.spray();
+            }, 1000 / frequency);
         }
     }
 
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
-            window.clearInterval(this.sprayInterval);
+            window.clearInterval(this.intervalID);
+            this.mouseDown = false;
         }
-        window.clearInterval(this.interval);
-        this.mouseDown = false;
     }
 
     onMouseMove(event: MouseEvent): void {
-        if (this.mouseDown) {
-            window.clearInterval(this.interval);
-            this.spray(event, this.drawingService.baseCtx);
-            this.interval = window.setInterval(() => {
-                this.spray(event, this.drawingService.baseCtx);
-            }, this.sprayInterval);
+        this.mouseCurrentPosition = this.getPositionFromMouse(event);
+    }
+
+    // onMouseLeave(event: MouseEvent): void {}
+
+    spray(): void {
+        this.generateSprayParticles();
+    }
+
+    generateSprayParticles(): void {
+        console.count('spray');
+        for (let i = 0; i < DOTS_PER_SPRAY; i++) {
+            const offset = this.getRandomOffsetInRadius((this.jetDiameter || MIN_JET_DIAMETER) / 2);
+            const radius = this.dropletDiameter || MIN_DROPLET_DIAMETER;
+            const currentPos = this.mouseCurrentPosition;
+            this.drawingService.baseCtx.strokeStyle = this.currentColourService.getPrimaryColorRgba();
+            this.drawingService.baseCtx.fillStyle = this.currentColourService.getPrimaryColorRgba();
+            this.drawingService.baseCtx.fillRect(currentPos.x + offset.x, currentPos.y + offset.y, radius, radius);
         }
     }
 
-    onMouseLeave(event: MouseEvent): void {
-        if (this.mouseDown) {
-            this.drawingService.previewCtx.beginPath();
-        }
-    }
-
-    spray(event: MouseEvent, ctx: CanvasRenderingContext2D): void {
-        // @ts-ignore
-        // tslint:disable:no-magic-numbers
-        const radius = this.dropletDiameter || MIN_AEROSOL_DROPLET_DIAMETER;
-        // const radius = 3;
-        const jetDiameter = this.jetDiameter || MIN_AEROSOL_JET_DIAMETER;
-        const dotsPerTick = this.emissionFlow || MIN_AEROSOL_EMISSION_FLOW;
-        const currentPos = this.getPositionFromMouse(event);
-        for (let i = 0; i < dotsPerTick / 10; i++) {
-            console.log('spray : ');
-            const offset = this.getRandomOffset(jetDiameter);
-            ctx.strokeStyle = this.currentColourService.getPrimaryColorRgba();
-            ctx.fillStyle = this.currentColourService.getPrimaryColorRgba();
-            ctx.fillRect(currentPos.x + offset.x, currentPos.y + offset.y, radius, radius);
-        }
-    }
-
-    generateSprayParticles(): void {}
-
-    getRandomOffset(radius: number): Vec2 {
+    getRandomOffsetInRadius(radius: number): Vec2 {
         const randomAngle = Math.random() * (2 * Math.PI);
         const randomRadius = Math.random() * radius;
-
-        // console.log(randomAngle, randomRadius, Math.cos(randomAngle), Math.sin(randomAngle));
-
         return {
             x: Math.cos(randomAngle) * randomRadius,
             y: Math.sin(randomAngle) * randomRadius,
         };
     }
-
-    // randomPointInRadius(radius: number): Vec2 {
-    //     for (;;) {
-    //         const x = Math.random() * 2 - 1;
-    //         const y = Math.random() * 2 - 1;
-    //         if (x * x + y * y <= 1) return { x: x * radius, y: y * radius };
-    //     }
-    // }
-
-    // generateSprayParticles(): void {
-    //     // Particle count, or, density
-    //     const density = 1;
-    //
-    //     for (let i = 0; i < density; i++) {
-    //         const offset = this.getRandomOffset(10);
-    //
-    //         // let x = mouse.x + offset.x;
-    //         // let y = mouse.y + offset.y;
-    //
-    //         // tmp_ctx.fillRect(x, y, 1, 1);
-    //     }
-    // }
 }
