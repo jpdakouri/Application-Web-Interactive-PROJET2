b23b6648daa08e396bc208b2f563508809c4aae9
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-02-27 16:07:44 -0500

    ajout des enums ImageFilter et ImageFormat

---
+++ client/src/app/utils/enums/image-filter.enum.ts
@@ -0,0 +1,9 @@
+export enum ImageFilter {
+    None = 'Aucun filtre',
+    Contrast = 'Contraste',
+    Blur = 'Flou',
+    Inversion = 'Inversion',
+    BlackAndWhite = 'Nuance de gris',
+    Saturation = 'Saturation des couleurs',
+    Sepia = 'Sépia',
+}
