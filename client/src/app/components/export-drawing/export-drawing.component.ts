cea8d8982f04617890fdea896e45c49ab02ca703
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-02-28 03:07:12 -0500

    Il est possible de voir une vignette de prévisualisation de l'image à exporter

--- client/src/app/components/export-drawing/export-drawing.component.ts
+++ client/src/app/components/export-drawing/export-drawing.component.ts
@@ -1,26 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { ExportDrawingService } from '@app/services/export-drawing/export-drawing.service';
+import { ImageFilter } from '@app/utils/enums/image-filter.enum';
+import { ImageFormat } from '@app/utils/enums/image-format.enum';
 
 @Component({
     selector: 'app-export-drawing',
     templateUrl: './export-drawing.component.html',
     styleUrls: ['./export-drawing.component.scss'],
 })
-export class ExportDrawingComponent implements OnInit {
-    extensions: string[] = ['PNG', 'JPEG'];
-    filters: string[] = ['Aucun filtre', 'Contraste', 'Flou', 'Inversion', 'Nuance de gris', 'Saturation des couleurs', 'Sepia'];
+export class ExportDrawingComponent implements OnInit, OnDestroy, AfterViewInit {
+    @ViewChild('previewCanvas', { static: false }) canvas: ElementRef<HTMLCanvasElement>;
 
-    constructor(public dialogRef: MatDialogRef<ExportDrawingComponent>) {}
+    selectedFormat: string;
+    selectedFilter: string;
+    filters: string[];
+    formats: string[];
+
+    constructor(private exportDrawingService: ExportDrawingService, public dialogRef: MatDialogRef<ExportDrawingComponent>) {
+        this.filters = Object.values(ImageFilter);
+        this.formats = Object.values(ImageFormat);
+        this.selectedFormat = ImageFormat.PNG;
+        this.selectedFilter = ImageFilter.None;
+    }
 
     ngOnInit(): void {}
+
+    ngAfterViewInit(): void {
+        this.exportDrawingService.previewCanvas = this.canvas.nativeElement as HTMLCanvasElement;
+        this.exportDrawingService.drawPreviewImage();
+    }
+
+    ngOnDestroy(): void {}
 
     exportDrawing(): void {}
 
     onDialogClose(): void {
-        this.dialogRef.close(ExportDrawingComponent);
+        this.dialogRef.close();
     }
 
     onDownload(): void {
+        this.exportDrawingService.exportImage();
         this.onDialogClose();
     }
+
+    onFormatChange(): void {}
+
+    onFilterChange(): void {}
 }
--- client/src/app/services/export-drawing/export-drawing.service.ts
+++ client/src/app/services/export-drawing/export-drawing.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { ImageFilter } from '@app/utils/enums/image-filter.enum';
+import { ImageFormat } from '@app/utils/enums/image-format.enum';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ExportDrawingService {
-    constructor() {}
+    previewCanvas: HTMLCanvasElement;
+    drawingTitle: string;
+    currentFilter: ImageFilter;
+    currentFormat: ImageFormat;
+
+    constructor() {
+        this.currentFilter = ImageFilter.None;
+        this.currentFormat = ImageFormat.JPEG;
+    }
+
+    convertCanvasToImage(canvas: HTMLCanvasElement): HTMLImageElement {
+        const image = new Image();
+        image.src = canvas.toDataURL('image/png');
+        console.log(image.src);
+        return image;
+    }
+
+    drawPreviewImage(): void {
+        const context = this.previewCanvas.getContext('2d') as CanvasRenderingContext2D;
+
+        // context.filter = 'sepia()';
+        // context.filter = 'blur(1px)';
+        const dataURL = sessionStorage.getItem('canvasBuffer');
+        const image = new Image();
+        if (dataURL) {
+            image.src = dataURL;
+            // tslint:disable-next-line:no-magic-numbers
+            context.scale(0.7, 0.7);
+            // image.style.filter = 'blur(5px)';
+            image.onload = () => {
+                context.drawImage(image, 0, 0);
+            };
+        }
+    }
+
+    exportImage(): void {}
 }
