5e4ae460140aa219b06b4218e0160348c63fd201
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-03-10 22:39:57 -0500

    ajout des imports dans les tests

--- client/src/app/components/export-drawing/export-drawing.component.spec.ts
+++ client/src/app/components/export-drawing/export-drawing.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MatDialogRef } from '@angular/material/dialog';
+// import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { ExportDrawingComponent } from './export-drawing.component';
 
@@ -13,6 +20,15 @@ describe('ExportDrawingComponent', () => {
         TestBed.configureTestingModule({
             declarations: [ExportDrawingComponent],
             providers: [{ provide: MatDialogRef, useValue: {} }],
+            imports: [
+                BrowserAnimationsModule,
+                MatOptionModule,
+                MatSelectModule,
+                MatDialogModule,
+                MatInputModule,
+                MatFormFieldModule,
+                ReactiveFormsModule,
+            ],
         }).compileComponents();
     }));
 
