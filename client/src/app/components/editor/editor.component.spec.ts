6890f1c3eda835a97ec1c0f6b90af820d0210f31
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-03-11 02:01:03 -0500

    réfactorisation de code

--- client/src/app/components/editor/editor.component.spec.ts
+++ client/src/app/components/editor/editor.component.spec.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
