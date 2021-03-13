b90611c6c1077347f330d6d5f5f04a106f626d9d
Jean-Philippe Dakouri <jean-philippe.dakouri@outlook.com>
2021-03-13 01:31:37 -0500

    selection ellipse marche toujours pas

--- client/src/app/services/tools/selectionEllipse-service/selection-ellipse.service.ts
+++ client/src/app/services/tools/selectionEllipse-service/selection-ellipse.service.ts
@@ -3,8 +3,10 @@ import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { CurrentColourService } from '@app/services/current-colour/current-colour.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { Sign } from '@app/services/services-constants';
 import { RectangleService } from '@app/services/tools/rectangle-service/rectangle.service';
-import { MouseButtons } from '@app/utils/enums/list-boutton-pressed';
+import { PIXELS_ARROW_STEPS } from '@app/services/tools/tools-constants';
+import { KeyboardButtons, MouseButtons } from '@app/utils/enums/list-boutton-pressed';
 
 // import { ShapeStyle } from '@app/utils/enums/shape-style';
 
@@ -17,28 +19,49 @@ const numberFifteen = 15;
 })
 export class SelectionEllipseService extends Tool {
     private firstGrid: Vec2;
-    // private shiftDown: boolean;
+    private topLeftCorner: Vec2;
+    private begin: Vec2;
+    private end: Vec2;
     private imageData: ImageData;
+    private shiftDown: boolean;
+    private selectionActive: boolean;
+
     rectangleService: RectangleService;
     currentColourService: CurrentColourService;
 
     constructor(drawingService: DrawingService, currentColourService: CurrentColourService) {
         super(drawingService, currentColourService);
         this.currentColourService = currentColourService;
+        this.topLeftCorner = { x: 0, y: 0 };
+        this.selectionActive = false;
     }
 
     onMouseDown(event: MouseEvent): void {
         this.clearPath();
         this.drawingService.clearCanvas(this.drawingService.previewCtx);
         this.mouseDown = event.button === MouseButtons.Left;
+        this.firstGrid = this.getPositionFromMouse(event);
+        this.mouseMoved = false;
         if (this.mouseDown) {
-            this.firstGrid = this.getPositionFromMouse(event);
-            this.updatePreview();
+            if (!this.selectionActive) {
+                this.begin = this.getPositionFromMouse(event);
+                this.updatePreview();
+                this.selectionActive = true;
+            } else {
+                if (this.isClickIn(this.firstGrid)) {
+                    console.log('click dedans');
+                } else {
+                    this.selectionActive = false;
+                    this.drawingService.baseCtx.putImageData(this.imageData, this.topLeftCorner.x, this.topLeftCorner.y);
+                    console.log('click dehors');
+                }
+            }
         }
     }
 
     onMouseMove(event: MouseEvent): void {
-        if (this.mouseDown) {
+        if (this.mouseDown && this.selectionActive) {
+            this.mouseMoved = true;
             this.mouseDownCoord.x = this.getPositionFromMouse(event).x - this.firstGrid.x;
             this.mouseDownCoord.y = this.getPositionFromMouse(event).y - this.firstGrid.y;
             this.updatePreview();
@@ -47,21 +70,64 @@ export class SelectionEllipseService extends Tool {
 
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
+            this.end = this.getPositionFromMouse(event);
+            this.updateTopLeftCorner();
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            if (this.shiftDown) {
+                this.drawSquare(this.mouseDownCoord);
+                this.selectEllipse(this.drawingService.previewCtx, this.mouseDownCoord);
+            }
+            this.selectEllipse(this.drawingService.previewCtx, this.mouseDownCoord);
             this.clearPath();
         }
-        this.selectEllipse(this.drawingService.previewCtx, this.mouseDownCoord);
-        this.clearPath();
         this.mouseDown = false;
     }
 
-    private updatePreview(): void {
+    onKeyDown(event: KeyboardEvent): void {
+        switch (event.key) {
+            case KeyboardButtons.Up: {
+                if (this.selectionActive) {
+                    this.topLeftCorner.y -= PIXELS_ARROW_STEPS;
+                }
+                break;
+            }
+            case KeyboardButtons.Down: {
+                if (this.selectionActive) {
+                    this.topLeftCorner.y += PIXELS_ARROW_STEPS;
+                }
+                break;
+            }
+            case KeyboardButtons.Right: {
+                if (this.selectionActive) {
+                    this.topLeftCorner.x += PIXELS_ARROW_STEPS;
+                }
+                break;
+            }
+            case KeyboardButtons.Left: {
+                if (this.selectionActive) {
+                    this.topLeftCorner.x -= PIXELS_ARROW_STEPS;
+                }
+                break;
+            }
+            case KeyboardButtons.Shift: {
+                this.shiftDown = true;
+                this.updatePreview();
+                break;
+            }
+            case KeyboardButtons.Escape: {
+                this.drawingService.clearCanvas(this.drawingService.previewCtx);
+                this.clearPath();
+            }
+        }
         this.drawingService.clearCanvas(this.drawingService.previewCtx);
-        const currentCoord = { ...this.mouseDownCoord };
-        this.drawingService.previewCtx.beginPath();
-        // const currentCoord = { ...this.mouseDownCoord };
-        this.drawRectanglePerimeter(this.drawingService.previewCtx, currentCoord);
-        this.drawingService.previewCtx.closePath();
+        this.drawingService.previewCtx.putImageData(this.imageData, this.topLeftCorner.x, this.topLeftCorner.y);
+    }
+
+    onKeyUp(event: KeyboardEvent): void {
+        if (this.shiftDown && event.key === KeyboardButtons.Shift) {
+            this.shiftDown = false;
+            this.updatePreview();
+        }
     }
 
     private drawRectanglePerimeter(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
@@ -81,21 +147,105 @@ export class SelectionEllipseService extends Tool {
         ctx.strokeRect(startCoord.x, startCoord.y, width, height);
     }
 
-    private selectEllipse(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
-        ctx.beginPath();
-        ctx.strokeStyle = this.currentColourService.getSecondaryColorRgba();
-        ctx.lineWidth = this.lineThickness = 1;
+    private isMouseInFirstQuadrant(): boolean {
+        //  mouse is in first quadrant (+/+)
+        return Math.sign(this.mouseDownCoord.x) === Sign.Positive && Math.sign(this.mouseDownCoord.y) === Sign.Positive;
+    }
 
+    private isMouseInSecondQuadrant(): boolean {
+        // mouse is in third quadrant (-/-)
+        return Math.sign(this.mouseDownCoord.x) === Sign.Negative && Math.sign(this.mouseDownCoord.y) === Sign.Negative;
+    }
+
+    private isMouseInThirdQuadrant(): boolean {
+        // mouse is in fourth quadrant (-/+)
+        return Math.sign(this.mouseDownCoord.x) === Sign.Negative && Math.sign(this.mouseDownCoord.y) === Sign.Positive;
+    }
+
+    private isMouseInFourthQuadrant(): boolean {
+        // mouse is in second quadrant (+/-)
+        return Math.sign(this.mouseDownCoord.x) === Sign.Positive && Math.sign(this.mouseDownCoord.y) === Sign.Negative;
+    }
+
+    private isXGreaterThanY(): boolean {
+        return Math.abs(this.mouseDownCoord.x) > Math.abs(this.mouseDownCoord.y);
+    }
+
+    private isYGreaterThanX(): boolean {
+        return Math.abs(this.mouseDownCoord.y) > Math.abs(this.mouseDownCoord.x);
+    }
+
+    private drawSquare(grid: Vec2): void {
+        if (this.isMouseInFirstQuadrant()) {
+            grid.x = grid.y = Math.min(this.mouseDownCoord.x, this.mouseDownCoord.y);
+        }
+
+        if (this.isMouseInSecondQuadrant()) {
+            grid.x = grid.y = Math.max(this.mouseDownCoord.x, this.mouseDownCoord.y);
+        }
+
+        if (this.isMouseInThirdQuadrant()) {
+            this.isXGreaterThanY() ? (grid.x = -grid.y) : (grid.y = -grid.x);
+        }
+
+        if (this.isMouseInFourthQuadrant()) {
+            this.isYGreaterThanX() ? (grid.y = -grid.x) : (grid.x = -grid.y);
+        }
+    }
+
+    private updateTopLeftCorner(): void {
+        if (this.begin.x > this.end.x) {
+            this.topLeftCorner.x = this.end.x;
+        }
+        if (this.begin.x < this.end.x) {
+            this.topLeftCorner.x = this.begin.x;
+        }
+        if (this.begin.y > this.end.y) {
+            this.topLeftCorner.y = this.end.y;
+        }
+        if (this.begin.y < this.end.y) {
+            this.topLeftCorner.y = this.begin.y;
+        }
+    }
+
+    private clippedRegion(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
         const startCoord = { ...this.firstGrid };
         const width = finalGrid.x;
         const height = finalGrid.y;
 
         ctx.ellipse(startCoord.x + width / 2, startCoord.y + height / 2, Math.abs(width / 2), Math.abs(height / 2), 0, 0, 2 * Math.PI, false);
-        ctx.stroke();
+        // ctx.stroke();
         ctx.clip();
+    }
 
+    private selectEllipse(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
+        ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = this.lineThickness = 1;
         this.imageData = this.drawingService.baseCtx.getImageData(this.firstGrid.x, this.firstGrid.y, finalGrid.x, finalGrid.y);
+        this.clippedRegion(ctx, finalGrid);
         ctx.putImageData(this.imageData, this.firstGrid.x, this.firstGrid.y);
+    }
+
+    private updatePreview(): void {
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
+        const currentCoord = { ...this.mouseDownCoord };
+        this.drawingService.previewCtx.beginPath();
+        if (this.shiftDown) {
+            this.drawSquare(currentCoord);
+        }
+        this.drawRectanglePerimeter(this.drawingService.previewCtx, currentCoord);
+        this.drawingService.previewCtx.closePath();
+    }
+
+    private isClickIn(firstGrid: Vec2): boolean {
+        if (firstGrid.x < this.topLeftCorner.x || firstGrid.x > this.topLeftCorner.x + this.imageData.width) {
+            return false;
+        }
+        if (firstGrid.y < this.topLeftCorner.y || firstGrid.y > this.topLeftCorner.y + this.imageData.height) {
+            return false;
+        }
+        return true;
     }
 
     private clearPath(): void {
