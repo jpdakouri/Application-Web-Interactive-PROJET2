969a21fd45be78a962f15ff1c28d0f9239df4465
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-03-19 13:14:38 -0400

    fixed attributes of polygon commands

--- client/src/app/classes/tool-commands/polygon-command.spec.ts
+++ client/src/app/classes/tool-commands/polygon-command.spec.ts
@@ -6,6 +6,17 @@ import { PolygonCommand } from './polygon-command';
 describe('PolygonCommand', () => {
     it('should create an instance', () => {
         const numberOfSides = 5;
-        expect(new PolygonCommand(TestBed.inject(PolygonService), '255,255,255,1', '0,0,0,1', numberOfSides, ShapeStyle.Filled, 2)).toBeTruthy();
+        expect(
+            new PolygonCommand(
+                TestBed.inject(PolygonService),
+                '255,255,255,1',
+                '0,0,0,1',
+                { x: 0, y: 0 },
+                { x: 100, y: 100 },
+                numberOfSides,
+                2,
+                ShapeStyle.Filled,
+            ),
+        ).toBeTruthy();
     });
 });
--- client/src/app/classes/tool-commands/polygon-command.ts
+++ client/src/app/classes/tool-commands/polygon-command.ts
@@ -1,26 +1,33 @@
+import { Vec2 } from '@app/classes/vec2';
 import { PolygonService } from '@app/services/tools/polygon-service/polygon.service';
 import { ShapeStyle } from '@app/utils/enums/shape-style';
 import { ToolCommand } from '@app/utils/interfaces/tool-command';
 
 export class PolygonCommand implements ToolCommand {
-    tool: PolygonService;
-    primaryColor: string;
-    secondaryColor: string;
-    numberOfSides: number;
-    shapeStyle: ShapeStyle;
-    strokeThickness: number;
+    readonly tool: PolygonService;
+    readonly primaryColor: string;
+    readonly secondaryColor: string;
+    readonly numberOfSides: number;
+    readonly shapeStyle?: ShapeStyle;
+    readonly strokeThickness: number;
+    readonly initialPosition: Vec2;
+    readonly finalPosition: Vec2;
 
     constructor(
         tool: PolygonService,
         primaryColor: string,
         secondaryColor: string,
+        initialPosition: Vec2,
+        finalPosition: Vec2,
         numberOfSides: number,
-        shapeStyle: ShapeStyle,
         strokeThickness: number,
+        shapeStyle?: ShapeStyle,
     ) {
         this.tool = tool;
         this.primaryColor = primaryColor;
         this.secondaryColor = secondaryColor;
+        this.initialPosition = initialPosition;
+        this.finalPosition = finalPosition;
         this.numberOfSides = numberOfSides;
         this.shapeStyle = shapeStyle;
         this.strokeThickness = strokeThickness;
