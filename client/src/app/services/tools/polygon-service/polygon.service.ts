84aa9a9c2f0223729618365035dc0975776876c5
Yohann Tahar <yohann.tahar@polymtl.ca>
2021-03-19 19:55:14 -0400

    Test fait pour polygone

--- client/src/app/services/tools/polygon-service/polygon.service.ts
+++ client/src/app/services/tools/polygon-service/polygon.service.ts
@@ -15,7 +15,7 @@ import { ToolCommand } from '@app/utils/interfaces/tool-command';
     providedIn: 'root',
 })
 export class PolygonService extends Tool {
-    private firstGrid: Vec2;
+    firstGrid: Vec2;
     numberOfSides: number;
     currentColourService: CurrentColourService;
     visualisationEllipse: EllipseService;
@@ -41,6 +41,7 @@ export class PolygonService extends Tool {
             this.mouseDownCoord.y = this.getPositionFromMouse(event).y - this.firstGrid.y;
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
             this.drawPolygon(this.drawingService.previewCtx, this.mouseDownCoord);
+            this.drawPreview(this.drawingService.previewCtx, this.mouseDownCoord);
         }
     }
 
@@ -73,9 +74,6 @@ export class PolygonService extends Tool {
         }
         ctx.closePath();
         ctx.stroke();
-        if (ctx === this.drawingService.previewCtx) {
-            this.drawPreview(ctx, finalGrid);
-        }
     }
 
     private drawFilled(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
@@ -98,9 +96,6 @@ export class PolygonService extends Tool {
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
-        if (ctx === this.drawingService.previewCtx) {
-            this.drawPreview(ctx, finalGrid);
-        }
     }
 
     private drawFilledOutLine(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
@@ -123,9 +118,6 @@ export class PolygonService extends Tool {
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
-        if (ctx === this.drawingService.previewCtx) {
-            this.drawPreview(ctx, finalGrid);
-        }
     }
 
     private drawPolygon(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
@@ -152,31 +144,31 @@ export class PolygonService extends Tool {
     private drawPreview(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
         ctx.setLineDash([LINE_DASH, LINE_DASH]);
         this.drawCircle(finalGrid);
-        let test: Vec2 = this.firstGrid;
+        let firstGrid: Vec2 = this.firstGrid;
         if (this.isMouseInFirstQuadrant()) {
             finalGrid.x += ctx.lineWidth;
             finalGrid.y += ctx.lineWidth;
-            test = { x: this.firstGrid.x - ctx.lineWidth / 2, y: this.firstGrid.y - ctx.lineWidth / 2 };
+            firstGrid = { x: this.firstGrid.x - ctx.lineWidth / 2, y: this.firstGrid.y - ctx.lineWidth / 2 };
         }
 
         if (this.isMouseInSecondQuadrant()) {
             finalGrid.x -= ctx.lineWidth;
             finalGrid.y -= ctx.lineWidth;
-            test = { x: this.firstGrid.x + ctx.lineWidth / 2, y: this.firstGrid.y + ctx.lineWidth / 2 };
+            firstGrid = { x: this.firstGrid.x + ctx.lineWidth / 2, y: this.firstGrid.y + ctx.lineWidth / 2 };
         }
 
         if (this.isMouseInThirdQuadrant()) {
             finalGrid.x -= ctx.lineWidth;
             finalGrid.y += ctx.lineWidth;
-            test = { x: this.firstGrid.x + ctx.lineWidth / 2, y: this.firstGrid.y - ctx.lineWidth / 2 };
+            firstGrid = { x: this.firstGrid.x + ctx.lineWidth / 2, y: this.firstGrid.y - ctx.lineWidth / 2 };
         }
 
         if (this.isMouseInFourthQuadrant()) {
             finalGrid.x += ctx.lineWidth;
             finalGrid.y -= ctx.lineWidth;
-            test = { x: this.firstGrid.x - ctx.lineWidth / 2, y: this.firstGrid.y + ctx.lineWidth / 2 };
+            firstGrid = { x: this.firstGrid.x - ctx.lineWidth / 2, y: this.firstGrid.y + ctx.lineWidth / 2 };
         }
-        this.visualisationEllipse.drawOutline(this.drawingService.previewCtx, test, finalGrid, 'blue', 1);
+        this.visualisationEllipse.drawOutline(this.drawingService.previewCtx, firstGrid, finalGrid, 'blue', 1);
     }
 
     private isMouseInFirstQuadrant(): boolean {
