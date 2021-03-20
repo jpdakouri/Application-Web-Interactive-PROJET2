1e23383fba3fb851417291dcba21cb2475ff9e69
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-03-20 15:30:29 -0400

    100% de couverture pour ExportDrawingService

--- client/src/app/services/export-drawing/export-drawing.service.spec.ts
+++ client/src/app/services/export-drawing/export-drawing.service.spec.ts
@@ -7,7 +7,6 @@ import { ExportDrawingService } from './export-drawing.service';
 describe('ExportDrawingService', () => {
     let service: ExportDrawingService;
     let canvasTestHelper: CanvasTestHelper;
-    // let link: HTMLAnchorElement;
     let spyLink: jasmine.SpyObj<HTMLAnchorElement>;
 
     beforeEach(() => {
@@ -24,15 +23,43 @@ describe('ExportDrawingService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('should be able to apply filter on canvas', () => {
+    it('#applyFilterOnCanvas should be able to apply filter on canvas', () => {
+        // tslint:disable:no-string-literal
         const filter = ImageFilter.Blur as string;
         const expectedFilterValue = 'blur(5px)' as string;
+
         service['applyFilterOnCanvas'](filter, service.canvas);
+
         const context = service.canvas.getContext('2d') as CanvasRenderingContext2D;
         expect(context.filter).toEqual(expectedFilterValue);
     });
 
+    it('#drawImageOnCanvas should be able to draw image on canvas', () => {
+        const image = new Image();
+        image.src = service.imageSource;
+
+        const context = service.canvas.getContext('2d') as CanvasRenderingContext2D;
+        const drawImageSpy = spyOn<any>(context, 'drawImage').and.callThrough();
+        const whiteColor = '#ffffff' as string;
+
+        service['drawImageOnCanvas'](image, service.canvas);
+
+        expect(context.fillStyle).toBe(whiteColor);
+        expect(drawImageSpy).toHaveBeenCalled();
+    });
+
+    it('#drawImageOnCanvas should be able to draw image on canvas with filter', () => {
+        const image = new Image();
+        image.src = service.imageSource;
+        const filter = ImageFilter.Sepia as string;
+        const applyFilterOnCanvasSpy = spyOn<any>(service, 'applyFilterOnCanvas').and.callThrough();
+        service['drawImageOnCanvas'](image, service.canvas, filter);
+
+        expect(applyFilterOnCanvasSpy).toHaveBeenCalled();
+    });
+
     it('#drawImageOnCanvas should not call #applyFilterOnCanvas if filter is undefined', () => {
+        // tslint:disable:no-any
         const applyFilterOnCanvasSpy = spyOn<any>(service, 'applyFilterOnCanvas').and.stub();
         const image = new Image();
         image.src = service.imageSource;
@@ -42,49 +69,58 @@ describe('ExportDrawingService', () => {
         expect(applyFilterOnCanvasSpy).not.toHaveBeenCalled();
     });
 
-    it('should be able to draw image on canvas with filter', () => {
-        const image = new Image();
-        image.src = service.imageSource;
-        const filter = ImageFilter.Sepia as string;
-        // const expectedFilterValue = 'blur(5px)' as string;
-        // tslint:disable:no-any
-        const context = service.canvas.getContext('2d') as CanvasRenderingContext2D;
-        const drawImageSpy = spyOn<any>(context, 'drawImage').and.callThrough();
-        const applyFilterOnCanvasSpy = spyOn<any>(service, 'applyFilterOnCanvas').and.callThrough();
-        const whiteColor = '#ffffff' as string;
-        service['drawImageOnCanvas'](image, service.canvas, filter);
-        expect(context.fillStyle).toBe(whiteColor);
-        expect(drawImageSpy).toHaveBeenCalled();
-        expect(applyFilterOnCanvasSpy).toHaveBeenCalled();
-        // expect(context.filter).toEqual(expectedFilterValue);
-    });
-
-    it('#drawImageOnCanvas should correctly set canvas size ', () => {
-        const imageWidth = 200;
+    it('#drawImageOnCanvas should correctly set download processing canvas size', () => {
+        const imageWidth = 100;
         const imageHeight = 200;
         const image = new Image(imageWidth, imageHeight);
+
         service['drawImageOnCanvas'](image, service.canvas);
+
         expect(service.canvas.width).toBe(imageWidth);
         expect(service.canvas.height).toBe(imageHeight);
     });
 
     it('should be able to download drawing as image', () => {
-        spyOn(document, 'createElement').and.returnValue(spyLink);
-        // const filter = ImageFilter.Blur as string;
-        // const expectedFilterValue = 'blur(5px)' as string;
-        // service['applyFilterOnCanvas'](filter, service.canvas);
-        // const context = service.canvas.getContext('2d') as CanvasRenderingContext2D;
-        // expect(context.filter).toEqual(expectedFilterValue);
         const drawImageSpy = spyOn<any>(service, 'drawImageOnCanvas').and.callThrough();
         const fileName = 'drawing_1';
-        const imageFormat = ImageFormat.PNG;
+        const imageFormat = ImageFormat.PNG as string;
+
         service['downloadDrawingAsImage'](fileName, imageFormat);
+
         const expectedImageSource = service.canvas.toDataURL('image/png') as string;
+
+        expect(drawImageSpy).toHaveBeenCalled();
         expect(spyLink.click).toHaveBeenCalled();
         expect(spyLink.click).toHaveBeenCalledTimes(1);
-        // expect(spyLink.click()).toHaveBeenCalledWith(fileName, ImageFormat.PNG);
         expect(spyLink.download).toBe(fileName);
         expect(spyLink.href).toBe(expectedImageSource);
-        expect(drawImageSpy).toHaveBeenCalled();
+    });
+
+    it('should be able to download image in JPEG format', () => {
+        const fileName = 'drawing-2';
+        const imageFormat = ImageFormat.JPEG as string;
+
+        service['downloadDrawingAsImage'](fileName, imageFormat);
+
+        const expectedImageSource = service.canvas.toDataURL('image/jpeg') as string;
+
+        expect(spyLink.click).toHaveBeenCalled();
+        expect(spyLink.click).toHaveBeenCalledTimes(1);
+        expect(spyLink.href).toBe(expectedImageSource);
+        expect(spyLink.download).toBe(fileName);
+    });
+
+    it('should be able to download image in PNG format', () => {
+        const fileName = 'Drawing_3';
+        const imageFormat = ImageFormat.PNG as string;
+
+        service['downloadDrawingAsImage'](fileName, imageFormat);
+
+        const expectedImageSource = service.canvas.toDataURL('image/png') as string;
+
+        expect(spyLink.click).toHaveBeenCalled();
+        expect(spyLink.click).toHaveBeenCalledTimes(1);
+        expect(spyLink.href).toBe(expectedImageSource);
+        expect(spyLink.download).toBe(fileName);
     });
 });
