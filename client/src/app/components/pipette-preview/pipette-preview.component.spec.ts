489b662deeabfbc0ec3486d0d7d5bc605747e580
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-03-21 20:27:23 -0400

    fix elementref undefined error in tests

--- client/src/app/components/pipette-preview/pipette-preview.component.spec.ts
+++ client/src/app/components/pipette-preview/pipette-preview.component.spec.ts
@@ -1,3 +1,4 @@
+import { ElementRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { DrawingService } from '@app/services/drawing/drawing.service';
@@ -30,7 +31,7 @@ describe('PipettePreviewComponent', () => {
         baseCanvasContext = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
         drawingService.baseCtx = baseCanvasContext;
         drawingService.canvas = canvasTestHelper.canvas;
-        component.pipettePreviewCanvas.nativeElement = canvasTestHelper.canvas;
+        component.pipettePreviewCanvas = new ElementRef<HTMLCanvasElement>(canvasTestHelper.canvas);
     });
 
     it('should create', () => {
@@ -42,15 +43,25 @@ describe('PipettePreviewComponent', () => {
         spyOn(service, 'getIsCursorOnCanvas').and.returnValue(false);
         const ctx = TestBed.inject(DrawingService).baseCtx;
         spyOn(ctx, 'fillRect');
+        component.onMouseMove();
         expect(ctx.fillRect).not.toHaveBeenCalled();
     });
 
     it('onMouseMove gets the preview colors and colors the canvas 225 times for each pixel', () => {
         const service = TestBed.inject(PipetteService);
         spyOn(service, 'getIsCursorOnCanvas').and.returnValue(true);
-        spyOn(service, 'getPreviewColors');
+        const previewColors: string[][] = [];
+        for (let i = 0; i < PREVIEW_SIZE; i++) {
+            const currentRow: string[] = [];
+            for (let j = 0; j < PREVIEW_SIZE; j++) {
+                currentRow.push('255,255,255,1');
+            }
+            previewColors.push(currentRow);
+        }
+        spyOn(service, 'getPreviewColors').and.returnValue(previewColors);
         spyOn(drawingService.baseCtx, 'fillRect');
         const expectedCallTimes = PREVIEW_SIZE * PREVIEW_SIZE;
+        component.onMouseMove();
         expect(service.getPreviewColors).toHaveBeenCalled();
         expect(drawingService.baseCtx.fillRect).toHaveBeenCalledTimes(expectedCallTimes);
     });
