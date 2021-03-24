a6d79fa88f713bb1edc18161063379e132bcfca8
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-03-23 20:12:25 -0400

    fix utils

--- client/src/app/utils/enums/rgb-settings.ts
+++ client/src/app/utils/enums/rgb-settings.ts
@@ -3,13 +3,13 @@ export enum RgbSettings {
     RGBA_START = 'rgba(',
     RGB_RGBA_END = ')',
     RGB_RGBA_SEPARATOR = ',',
-    RGB_NUMBER_OF_COLOURS = 3,
+    RGB_NUMBER_OF_COLORS = 3,
     DEFAULT_PRIMARY_RGB = '0,0,0',
     DEFAULT_SECONDARY_RGB = '255,255,255',
     DEFAULT_TRANSPARENCY = '1',
 }
 
-export const DEFAULT_COLOUR = RgbSettings.RGB_START + RgbSettings.DEFAULT_SECONDARY_RGB + RgbSettings.RGB_RGBA_END;
+export const DEFAULT_COLOR = RgbSettings.RGB_START + RgbSettings.DEFAULT_SECONDARY_RGB + RgbSettings.RGB_RGBA_END;
 
 export enum Sign {
     Negative = -1,
