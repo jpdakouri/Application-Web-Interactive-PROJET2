483a57fb91149b26d090e2844955c995b3493763
Moustapha Dienta <moustapha5dienta@gmail.com>
2021-03-26 03:17:53 -0400

    ajout d'un component pour afficher lien de téléversement

---
+++ client/src/app/components/export-drawing/upload-link/upload-link.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UploadLinkComponent } from './upload-link.component';
+
+describe('UploadLinkComponent', () => {
+    let component: UploadLinkComponent;
+    let fixture: ComponentFixture<UploadLinkComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UploadLinkComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UploadLinkComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
