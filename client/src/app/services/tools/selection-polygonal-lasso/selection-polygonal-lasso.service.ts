ddce62ae1af63ff4f4001f268780cd71197f32a9
Elion <erushiti@hotmail.com>
2021-04-03 14:38:18 -0400

    debut de clip

--- client/src/app/services/tools/selection-polygonal-lasso/selection-polygonal-lasso.service.ts
+++ client/src/app/services/tools/selection-polygonal-lasso/selection-polygonal-lasso.service.ts
@@ -45,18 +45,17 @@ export class SelectionPolygonalLassoService extends LineCreatorService {
         return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
     }
     // varifies lines aren't superposed
-    private superposedLines(line1: Vec2[], line2: Vec2[]): boolean {
-        const a = (line1[1].y - line1[0].y) / (line1[1].x - line1[0].x);
-        const b = line1[1].y - a * line1[1].x;
-        const superposedPoint1 = line2[0].y === a * line2[0].x + b;
-        const superposedPoint2 = line2[1].y === a * line2[1].x + b;
-        return superposedPoint1 && superposedPoint2;
-    }
-
+    // private superposedLines(line1: Vec2[], line2: Vec2[]): boolean {
+    //     const a = (line1[1].y - line1[0].y) / (line1[1].x - line1[0].x);
+    //     const b = line1[1].y - a * line1[1].x;
+    //     const superposedPoint1 = line2[0].y === a * line2[0].x + b;
+    //     const superposedPoint2 = line2[1].y === a * line2[1].x + b;
+    //     return superposedPoint1 && superposedPoint2;
+    // }
     // algorithm found at https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
     private verifiyCrossedLines(line1: Vec2[], line2: Vec2[]): boolean {
         return (
-            this.superposedLines(line1, line2) &&
+            // this.superposedLines(line1, line2) &&
             this.ccw(line1[0], line2[0], line2[1]) !== this.ccw(line1[1], line2[0], line2[1]) &&
             this.ccw(line1[0], line1[1], line2[0]) !== this.ccw(line1[0], line1[1], line2[1])
         );
@@ -77,7 +76,46 @@ export class SelectionPolygonalLassoService extends LineCreatorService {
             this.lineThickness || DEFAULT_MIN_THICKNESS,
             closedSegment,
         );
+        this.drawingService.baseCtx.save();
+        this.clipArea();
+        this.drawingService.baseCtx.restore();
         this.clearPath();
+    }
+
+    clipArea(): void {
+        this.drawingService.clearCanvas(this.drawingService.selectedAreaCtx);
+        this.drawingService.baseCtx.beginPath();
+        this.drawingService.baseCtx.moveTo(this.pathData[0].x, this.pathData[0].y);
+        for (const point of this.pathData) this.drawingService.baseCtx.lineTo(point.x, point.y);
+
+        this.drawingService.baseCtx.lineTo(this.pathData[0].x, this.pathData[0].y);
+        this.drawingService.baseCtx.stroke();
+        this.drawingService.baseCtx.clip();
+
+        const coords = this.getClippedCoords();
+        const size = this.getClippedSize(coords);
+        this.drawingService.selectedAreaCtx.translate(-coords[0].x, -coords[0].y);
+        this.drawingService.selectedAreaCtx.canvas.height = size.y;
+        this.drawingService.selectedAreaCtx.canvas.width = size.x;
+        this.drawingService.selectedAreaCtx.putImageData(this.drawingService.baseCtx.getImageData(coords[0].x, coords[0].y, size.x, size.y), 0, 0);
+    }
+
+    getClippedSize(coords: Vec2[]): Vec2 {
+        const height = Math.abs(coords[1].y - coords[0].y);
+        const width = Math.abs(coords[1].x - coords[0].x);
+        return { x: width, y: height } as Vec2;
+    }
+
+    getClippedCoords(): Vec2[] {
+        let maxCoords = { x: 0, y: 0 } as Vec2;
+        let minCoords = { x: 1000, y: 1000 } as Vec2;
+
+        for (const point of this.pathData) {
+            maxCoords = { x: Math.max(maxCoords.x, point.x), y: Math.max(maxCoords.y, point.y) } as Vec2;
+            minCoords = { x: Math.min(minCoords.x, point.x), y: Math.min(minCoords.y, point.y) } as Vec2;
+        }
+
+        return [minCoords, maxCoords];
     }
 
     // No need here
