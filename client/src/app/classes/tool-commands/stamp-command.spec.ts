7bd70fe51d0d14d6fadf364044673e9ef99a8928
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-04-05 15:37:08 -0400

    added all files for stamp

---
+++ client/src/app/classes/tool-commands/stamp-command.spec.ts
@@ -0,0 +1,7 @@
+import { StampCommand } from './stamp-command';
+
+describe('StampCommand', () => {
+  it('should create an instance', () => {
+    expect(new StampCommand()).toBeTruthy();
+  });
+});
