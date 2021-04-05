17b6a827de63602aaf2b6c898b787c7b9e51ba8d
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-04-05 15:45:23 -0400

    command done

--- client/src/app/classes/tool-commands/stamp-command.spec.ts
+++ client/src/app/classes/tool-commands/stamp-command.spec.ts
@@ -1,7 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { StampService } from '@app/services/tools/stamp-service/stamp.service';
 import { StampCommand } from './stamp-command';
 
 describe('StampCommand', () => {
-  it('should create an instance', () => {
-    expect(new StampCommand()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        const tool = TestBed.inject(StampService);
+        expect(new StampCommand(tool, 1, 1, { x: 1, y: 1 })).toBeTruthy();
+    });
 });
--- client/src/app/classes/tool-commands/stamp-command.ts
+++ client/src/app/classes/tool-commands/stamp-command.ts
@@ -1,2 +1,16 @@
-export class StampCommand {
+import { Vec2 } from '@app/classes/vec2';
+import { StampService } from '@app/services/tools/stamp-service/stamp.service';
+import { ToolCommand } from '@app/utils/interfaces/tool-command';
+
+export class StampCommand implements ToolCommand {
+    readonly tool: StampService;
+    readonly scalingFactor: number;
+    readonly rotationAngle: number;
+    readonly center: Vec2;
+    constructor(tool: StampService, scalingFactor: number, rotationAngle: number, center: Vec2) {
+        this.tool = tool;
+        this.scalingFactor = scalingFactor;
+        this.rotationAngle = rotationAngle;
+        this.center = center;
+    }
 }
