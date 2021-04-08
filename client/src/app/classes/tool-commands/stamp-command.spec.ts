515f5cb88cdd29167c0b349437df63d5c85e31d2
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-04-08 18:17:57 -0400

    fixed test for command

--- client/src/app/classes/tool-commands/stamp-command.spec.ts
+++ client/src/app/classes/tool-commands/stamp-command.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { StampService } from '@app/services/tools/stamp-service/stamp.service';
+import { Stamp } from '@app/utils/enums/stamp';
 import { StampCommand } from './stamp-command';
 
 describe('StampCommand', () => {
     it('should create an instance', () => {
         const tool = TestBed.inject(StampService);
-        expect(new StampCommand(tool, 1, 1, { x: 1, y: 1 })).toBeTruthy();
+        expect(new StampCommand(tool, 1, 1, { x: 1, y: 1 }, Stamp.House)).toBeTruthy();
     });
 });
