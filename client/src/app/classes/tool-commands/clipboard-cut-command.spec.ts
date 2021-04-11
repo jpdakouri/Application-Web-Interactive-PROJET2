c5bbbd84c32c49c6a2b5a88f79325b7ebb2c7626
chsiga <christopher.sigouin-bond@polymtl.ca>
2021-04-11 16:16:25 -0400

    removed cut command

--- client/src/app/classes/tool-commands/clipboard-cut-command.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ClipboardCutCommand } from './clipboard-cut-command';
-
-describe('ClipboardCutCommand', () => {
-  it('should create an instance', () => {
-    expect(new ClipboardCutCommand()).toBeTruthy();
-  });
-});
--- client/src/app/classes/tool-commands/clipboard-delete-command.ts
+++ client/src/app/classes/tool-commands/clipboard-delete-command.ts
@@ -1,2 +1,12 @@
-export class ClipboardDeleteCommand {
+import {Vec2} from '@app/classes/vec2';
+import { ClipboardService } from '@app/services/tools/clipboard-service/clipboard.service';
+import { ToolCommand } from '@app/utils/interfaces/tool-command';
+
+export class ClipboardPasteCommand implements ToolCommand {
+    readonly tool: ClipboardService; //TODO: changer pour sélection
+    readonly selectionPositions: Vec2[];
+    constructor(tool: ClipboardService, selectionPositions: Vec2[]) {
+        this.tool = tool;
+        this.selectionPositions = selectionPositions;
+    }
 }
