5de00839439840f88927f7b0f7b2ad2f268ab7ca
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-04-13 18:57:02 -0400

    fixed delete command imports

--- client/src/app/classes/tool-commands/clipboard-delete-command.ts
+++ client/src/app/classes/tool-commands/clipboard-delete-command.ts
@@ -1,9 +1,9 @@
-import {Vec2} from '@app/classes/vec2';
+import { Vec2 } from '@app/classes/vec2';
 import { ClipboardService } from '@app/services/tools/clipboard-service/clipboard.service';
 import { ToolCommand } from '@app/utils/interfaces/tool-command';
 
-export class ClipboardPasteCommand implements ToolCommand {
-    readonly tool: ClipboardService; //TODO: changer pour sélection
+export class ClipboardDeleteCommand implements ToolCommand {
+    readonly tool: ClipboardService; // TODO: changer pour sélection
     readonly selectionPositions: Vec2[];
     constructor(tool: ClipboardService, selectionPositions: Vec2[]) {
         this.tool = tool;
