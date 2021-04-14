a2877c2110a4401fd3c11e4782edd8073a2539dd
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-04-14 19:28:31 -0400

    lint fix

--- client/src/app/services/tools/clipboard-service/clipboard.service.spec.ts
+++ client/src/app/services/tools/clipboard-service/clipboard.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ClipboardService } from './clipboard.service';
 
 describe('ClipboardService', () => {
-  let service: ClipboardService;
+    let service: ClipboardService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ClipboardService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ClipboardService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
