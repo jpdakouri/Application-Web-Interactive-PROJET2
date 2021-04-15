6c148663037f7e59e83962b241f459f59fe793d0
Jean-Philippe Dakouri <jean-philippe.dakouri@outlook.com>
2021-04-15 16:26:12 -0400

    Touche directionnelles partiellement fonctionnelle

--- client/src/app/services/tools/magnetism-service/magnetism.service.ts
+++ client/src/app/services/tools/magnetism-service/magnetism.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Vec2 } from '@app/classes/vec2';
 import { CurrentColorService } from '@app/services/current-color/current-color.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { GridService } from '@app/services/grid/grid.service';
@@ -7,13 +8,13 @@ import { SelectionService } from '@app/services/tools/selection-service/selectio
 import { KeyboardButtons } from '@app/utils/enums/keyboard-button-pressed';
 import { ToolCommand } from '@app/utils/interfaces/tool-command';
 
-const DISTANCE_TEST = 1;
 @Injectable({
     providedIn: 'root',
 })
 export class MagnetismService extends SelectionService {
     isMagnetismActivated: boolean;
     gridService: GridService;
+    mousePositionHandler: MousePositionHandlerService;
     constructor(
         drawingService: DrawingService,
         currentColorService: CurrentColorService,
@@ -22,15 +23,14 @@ export class MagnetismService extends SelectionService {
     ) {
         super(drawingService, currentColorService);
         this.gridService = gridService;
+        this.mousePositionHandler = mousePositionHandler;
     }
 
-    // onMouseMove(event: MouseEvent): void {
-
-    // }
+    onMouseMove(event: MouseEvent): void {
+        this.updatePreview();
+    }
 
     onKeyDown(event: KeyboardEvent): void {
-        this.isMagnetismActivated = true;
-        // this.firstGrid = this.getPositionFromMouse()
         switch (event.key) {
             case KeyboardButtons.Up: {
                 if (SelectionService.selectionActive) {
@@ -62,11 +62,28 @@ export class MagnetismService extends SelectionService {
 
     onKeyUp(event: KeyboardEvent): void {
         this.defaultOnKeyUp(event);
+        this.updatePreview();
     }
 
-    keepWithinGridDimension(): void {}
+    setCoordToNearestCrossOnGrid(coord: Vec2): void {
+        if (coord.x % this.gridService.gridSize) {
+            coord.x = coord.x + (this.gridService.gridSize - (coord.x % this.gridService.gridSize));
+        }
+
+        if (coord.y % this.gridService.gridSize) {
+            coord.y = coord.y + (this.gridService.gridSize - (coord.y % this.gridService.gridSize));
+        }
+    }
+
+    updateCoordToCurrentValues(): void {
+        this.firstGrid = { ...this.mouseDownCoord };
+        this.topLeftCorner = { ...this.initialTopLeftCorner };
+        // this.topLeftCorner = { x: 0, y: 0 };
+        // this.updateTopLeftCorner();
+    }
 
     updateArrowPositionMagnetism(): void {
+        // this.updateCoordToCurrentValues();
         if (SelectionService.selectionActive && this.upPressed) {
             this.topLeftCorner.y -= this.gridService.gridSize;
             this.firstGrid.y -= this.gridService.gridSize;
@@ -83,8 +100,44 @@ export class MagnetismService extends SelectionService {
             this.topLeftCorner.x -= this.gridService.gridSize;
             this.firstGrid.x -= this.gridService.gridSize;
         }
+        // this.setCoordToNearestCrossOnGrid({
+        //     x: this.drawingService.selectedAreaCtx.canvas.width,
+        //     y: this.drawingService.selectedAreaCtx.canvas.height,
+        // });
+        console.log('grid clean ? ' + this.topLeftCorner.x + ' ' + this.topLeftCorner.y);
+        // this.setCoordToNearestCrossOnGrid(this.firstGrid);
         this.drawingService.selectedAreaCtx.canvas.style.top = this.topLeftCorner.y - 1 + 'px';
         this.drawingService.selectedAreaCtx.canvas.style.left = this.topLeftCorner.x - 1 + 'px';
+    }
+
+    // verifies if B and C are counter clock wise from A
+    // private ccw(A: Vec2, B: Vec2, C: Vec2): boolean {
+    //     return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
+    // }
+
+    // // algorithm found at https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+    // private verifiyCrossedLines(line1: Vec2[], line2: Vec2[]): boolean {
+    //     return (
+    //         this.ccw(line1[0], line2[0], line2[1]) !== this.ccw(line1[1], line2[0], line2[1]) &&
+    //         this.ccw(line1[0], line1[1], line2[0]) !== this.ccw(line1[0], line1[1], line2[1])
+    //     );
+    // }
+
+    private drawRectanglePerimeter(ctx: CanvasRenderingContext2D, finalGrid: Vec2): void {
+        ctx.strokeStyle = 'blue';
+        ctx.lineWidth = 1;
+
+        const startCoord = { ...this.firstGrid };
+        const width = Math.abs(finalGrid.x);
+        const height = Math.abs(finalGrid.y);
+
+        if (finalGrid.x < 0) {
+            startCoord.x += finalGrid.x;
+        }
+        if (finalGrid.y < 0) {
+            startCoord.y += finalGrid.y;
+        }
+        ctx.strokeRect(startCoord.x, startCoord.y, width, height);
     }
 
     executeCommand(command: ToolCommand): void {
@@ -94,7 +147,15 @@ export class MagnetismService extends SelectionService {
     registerUndo(imageData: ImageData): void {
         throw new Error('Method not implemented.');
     }
+
     updatePreview(): void {
-        throw new Error('Method not implemented.');
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
+        const currentCoord = { ...this.mouseDownCoord };
+        this.drawingService.previewCtx.beginPath();
+        if (this.shiftDown) {
+            this.mousePositionHandler.makeSquare(this.mouseDownCoord, currentCoord);
+        }
+        this.drawRectanglePerimeter(this.drawingService.previewCtx, currentCoord);
+        this.drawingService.previewCtx.closePath();
     }
 }
