068f55d48e655f7e4a75ec3f5c7b66459157f4da
g0goTBC <christopher.sigouin-bond@polymtl.ca>
2021-04-15 18:28:49 -0400

    clipboard is no longer a tool

--- client/src/app/services/clipboard-service/clipboard.service.ts
+++ client/src/app/services/clipboard-service/clipboard.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Tool } from '@app/classes/tool';
-import { CurrentColorService } from '@app/services/current-color/current-color.service';
-import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ToolManagerService } from '@app/services/tool-manager/tool-manager.service';
 import { SelectionRectangleService } from '@app/services/tools/selection-rectangle-service/selection-rectangle.service';
 import { ToolsNames } from '@app/utils/enums/tools-names';
@@ -10,13 +7,9 @@ import { ToolCommand } from '@app/utils/interfaces/tool-command';
 @Injectable({
     providedIn: 'root',
 })
-export class ClipboardService extends Tool {
+export class ClipboardService {
     private clipboardContent?: ImageData;
-    private toolManager: ToolManagerService;
-    constructor(drawingService: DrawingService, currentColorService: CurrentColorService, toolManager: ToolManagerService) {
-        super(drawingService, currentColorService);
-        this.toolManager = toolManager;
-    }
+    constructor(private toolManager: ToolManagerService) {}
 
     copy(): void {
         const selectionTool = this.toolManager.getCurrentSelectionTool();
